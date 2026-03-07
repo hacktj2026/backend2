bee4533a1b0bc562288c3ad87a2628695fee8b96
Arnav Choudhary <choudharyarnav356@gmail.com>
2026-03-07 14:54:31 -0500

    Added problemBuilder

---
+++ hacktj/src/main/java/com/example/hacktj/problemBuilder.java
@@ -0,0 +1,15 @@
+package com.example.hacktj;
+import com.example.hacktj.model.Word;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import java.io.File;
+
+public class problemBuilder {
+    Word word;
+
+    public problemBuilder(Word word) {
+        this.word = word;
+    }
+    
+}
