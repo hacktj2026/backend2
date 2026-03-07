0249dcc384bd5670abd339dc089e416c6ae833ec
Mythofs <117866375+Mythofs@users.noreply.github.com>
2026-03-07 14:02:59 -0500

    Added spring boot template

---
+++ hacktj/src/test/java/com/example/hacktj/HacktjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hacktj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HacktjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
