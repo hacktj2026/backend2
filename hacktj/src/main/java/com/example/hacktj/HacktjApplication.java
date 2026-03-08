2c8b6503e98d886850b3596508d5a207bc3c6ee8
donutaskwhy787-cmyk <donutaskwhy787@gmail.com>
2026-03-07 23:17:52 -0500

    fdasadfsad

--- hacktj/src/main/java/com/example/hacktj/HacktjApplication.java
+++ hacktj/src/main/java/com/example/hacktj/HacktjApplication.java
@@ -96,21 +96,25 @@ public class HacktjApplication {
          correctAnswer = Conjugation.conjugatePresent(word, request.getPronoun());
           correct = correctAnswer.equals(request.getSelected());
           wordService.rightOrWrong(correct, word.getLevel(), userRepository.findByName(request.getUsername()));
+          userRepository.findByName(request.getUsername()).updateSkill(word, correct);
           return new AnswerResponse(correct);
         case "future":
           correctAnswer = Conjugation.conjugateFuture(word, request.getPronoun());
           correct = correctAnswer.equals(request.getSelected());
           wordService.rightOrWrong(correct, word.getLevel(), userRepository.findByName(request.getUsername()));
+          userRepository.findByName(request.getUsername()).updateSkill(word, correct);
           return new AnswerResponse(correct);
         case "conditional":
           correctAnswer = Conjugation.conjugateConditional(word, request.getPronoun());
           correct = correctAnswer.equals(request.getSelected());
           wordService.rightOrWrong(correct, word.getLevel(), userRepository.findByName(request.getUsername()));
+          userRepository.findByName(request.getUsername()).updateSkill(word, correct);
           return new AnswerResponse(correct);
       }
     }
     correct = word.getMeaning().equals(request.getSelected());
     wordService.rightOrWrong(correct, word.getLevel(), userRepository.findByName(request.getUsername()));
+    userRepository.findByName(request.getUsername()).updateSkill(word, correct);
     return new AnswerResponse(correct);
   }
   @DeleteMapping("/users")
@@ -196,6 +200,7 @@ public class HacktjApplication {
     
     correct = word.getMeaning().equals(request.getSelected());
     wordService.rightOrWrong(correct, word.getLevel(), userRepository.findByName(request.getUsername()));
+    userRepository.findByName(request.getUsername()).updateSkill(word, correct);
     return new AnswerResponse(correct);
   }
 
@@ -212,16 +217,19 @@ public class HacktjApplication {
          correctAnswer = Conjugation.conjugatePresent(word, request.getPronoun());
           correct = correctAnswer.equals(request.getSelected());
           wordService.rightOrWrong(correct, word.getLevel(), userRepository.findByName(request.getUsername()));
+          userRepository.findByName(request.getUsername()).updateSkill(word, correct);
           return new AnswerResponse(correct);
         case "future":
           correctAnswer = Conjugation.conjugateFuture(word, request.getPronoun());
           correct = correctAnswer.equals(request.getSelected());
           wordService.rightOrWrong(correct, word.getLevel(), userRepository.findByName(request.getUsername()));
+          userRepository.findByName(request.getUsername()).updateSkill(word, correct);
           return new AnswerResponse(correct);
         case "conditional":
           correctAnswer = Conjugation.conjugateConditional(word, request.getPronoun());
           correct = correctAnswer.equals(request.getSelected());
           wordService.rightOrWrong(correct, word.getLevel(), userRepository.findByName(request.getUsername()));
+          userRepository.findByName(request.getUsername()).updateSkill(word, correct);
           return new AnswerResponse(correct);
       }
 
