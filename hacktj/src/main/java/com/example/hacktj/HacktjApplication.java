373e324123760179c7777ebb8e8921e97c7e5a96
donutaskwhy787-cmyk <donutaskwhy787@gmail.com>
2026-03-07 20:32:55 -0500

    buildedr

--- hacktj/src/main/java/com/example/hacktj/HacktjApplication.java
+++ hacktj/src/main/java/com/example/hacktj/HacktjApplication.java
@@ -2,9 +2,17 @@ package com.example.hacktj;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-import com.example.hacktj.service.WordService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.hacktj.model.Word;
+import com.example.hacktj.model.problemBuilder;
+import com.example.hacktj.service.WordService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
@@ -29,6 +37,7 @@ public class HacktjApplication {
     problemBuilder builder = new problemBuilder(word);
     String problemJson = builder.problem();
     var problemData = mapper.readTree(problemJson);
+    
     
     String[] choices = mapper.convertValue(problemData.get("choices"), String[].class);
     
---
+++ hacktj/src/main/java/com/example/hacktj/model/Builder.java
@@ -0,0 +1,20 @@
+package com.example.hacktj.model;
+
+public abstract class Builder {
+    abstract String problem() throws Exception;
+    protected static int changeDiffLevel(String s) {
+        System.out.println(s);
+        switch(s) {
+            case "A1": return 0;
+            case "A2": return 1;
+            case "B1": return 2;
+            case "B2": return 3;
+            case "C1": return 4;
+            case "C2": return 5;
+            default: return -1;
+        }
+    }
+
+    
+
+}
